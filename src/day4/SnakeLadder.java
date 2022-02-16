01e8c366ae30763dbdb508fb835dfb062d4d7a06
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-02-16 17:32:00 +0530

    Getting random number between one to six

---
+++ src/day4/SnakeLadder.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		int start = 0;
+		System.out.println("Starting position of player is: " + start);
+		int roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		System.out.println(roll);
+
+	}
+
+}
