45ee50492cf5507f3a6855eed20f68002b5d02c5
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-02-16 17:12:29 +0530

    Game for Single Player is initialized

---
+++ src/day4/SnakeLadder.java
@@ -0,0 +1,11 @@
+package day4;
+
+public class SnakeLadder {
+	
+	public static void main(String[] args) {
+		int start = 0;
+		System.out.println("Starting position of player is: " + start);
+
+	}
+
+}
